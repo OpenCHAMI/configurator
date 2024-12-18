41aa0c7a42678ca1a6a23f4eb7cdfee9dddef97b
David J. Allen <davidallendj@gmail.com>
2024-12-18 16:22:46 -0700

    plugin: updated coredhcp to match interface

--- pkg/generator/coredhcp.go
+++ pkg/generator/coredhcp.go
@@ -1,12 +1,9 @@
-//go:build coredhcp || plugins
-// +build coredhcp plugins
-
 package generator
 
 import (
 	"fmt"
 
-	configurator "github.com/OpenCHAMI/configurator/pkg"
+	"github.com/OpenCHAMI/configurator/pkg/config"
 	"github.com/OpenCHAMI/configurator/pkg/util"
 )
 
@@ -24,6 +21,6 @@ func (g *CoreDhcp) GetDescription() string {
 	return fmt.Sprintf("Configurator generator plugin for '%s' to generate config files. (WIP)", g.GetName())
 }
 
-func (g *CoreDhcp) Generate(config *configurator.Config, opts ...util.Option) (FileMap, error) {
+func (g *CoreDhcp) Generate(config *config.Config, params Params) (FileMap, error) {
 	return nil, fmt.Errorf("plugin does not implement generation function")
 }
